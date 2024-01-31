5471a7a9b901b56c4c1fd7139f385982e67ed419
Steven Goulet <steven.goulet@live.com>
2024-01-31 14:35:10 -0500

    Refactor gallery.js for improved performance and readability

--- assets/myjs/gallery-old.js
+++ assets/myjs/gallery-old.js
@@ -1,58 +1,67 @@
-document.addEventListener('DOMContentLoaded', function () {
+
+    document.addEventListener('DOMContentLoaded', function() {
     var currentPage = 1;
     var pages = document.querySelectorAll('.page');
     var links = document.querySelectorAll('.page-link');
     var prevPageLinks = document.querySelectorAll('#prev-page-top, #prev-page-bottom');
     var nextPageLinks = document.querySelectorAll('#next-page-top, #next-page-bottom');
     var total_pages = parseInt(document.querySelector('#pagination-top').getAttribute('data-total-pages'));
+
     var images = document.querySelectorAll('img[data-src]');
     var config = {
         rootMargin: '0px 0px 50px 0px',
         threshold: 0
     };
-    var imageObserver = new IntersectionObserver(function (entries, self) {
-        entries.forEach(function (entry) {
+
+    var imageObserver = new IntersectionObserver(function(entries, self) {
+        entries.forEach(function(entry) {
             if (entry.isIntersecting) {
                 preloadImage(entry.target);
                 self.unobserve(entry.target);
             }
         });
     }, config);
-    images.forEach(function (image) {
+
+    images.forEach(function(image) {
         imageObserver.observe(image);
     });
+
     function preloadImage(img) {
-        var src = img.getAttribute('data-src');
+        const src = img.getAttribute('data-src');
         if (!src) {
             return;
         }
         img.src = src;
     }
+
     function showPage(page) {
-        pages.forEach(function (pageDiv) {
-            pageDiv.style.display = pageDiv.getAttribute('data-page') == page.toString() ? 'block' : 'none';
+        pages.forEach(function(pageDiv) {
+            pageDiv.style.display = pageDiv.getAttribute('data-page') == page ? 'block' : 'none';
         });
         currentPage = page;
-        prevPageLinks.forEach(function (link) {
+        prevPageLinks.forEach(function(link) {
             link.style.visibility = currentPage == 1 ? 'hidden' : 'visible';
         });
-        nextPageLinks.forEach(function (link) {
+        nextPageLinks.forEach(function(link) {
             link.style.visibility = currentPage == total_pages ? 'hidden' : 'visible';
         });
+
         // Remove the current-page class from all links
-        links.forEach(function (link) {
+        links.forEach(function(link) {
             link.classList.remove('current-page');
         });
+
         // Add the current-page class to the current page links
         var currentPageLinks = document.querySelectorAll('.page-link[data-page="' + currentPage + '"]');
         if (currentPageLinks) {
-            currentPageLinks.forEach(function (link) {
+            currentPageLinks.forEach(function(link) {
                 link.classList.add('current-page');
             });
         }
     }
-    links.forEach(function (link) {
-        link.addEventListener('click', function (e) {
+
+    links.forEach(function(link) {
+        link.addEventListener('click', function(e) {
             e.preventDefault();
             var page = parseInt(link.getAttribute('data-page')); // Convert the page attribute to a number
             if (page) {
@@ -60,25 +69,26 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         });
     });
-    prevPageLinks.forEach(function (link) {
-        link.addEventListener('click', function (e) {
+
+    prevPageLinks.forEach(function(link) {
+        link.addEventListener('click', function(e) {
             e.preventDefault();
-            if (currentPage > 1)
-                showPage(currentPage - 1);
+            if (currentPage > 1) showPage(currentPage - 1);
         });
     });
-    nextPageLinks.forEach(function (link) {
-        link.addEventListener('click', function (e) {
+
+    nextPageLinks.forEach(function(link) {
+        link.addEventListener('click', function(e) {
             e.preventDefault();
-            if (currentPage < total_pages)
-                showPage(currentPage + 1);
+            if (currentPage < total_pages) showPage(currentPage + 1);
         });
     });
     showPage(currentPage);
 });
+
 function scrollToTop() {
     window.scrollTo({
         top: 0,
         behavior: 'smooth'
     });
-}
+}    
---
+++ assets/myjs/gallery.ts
@@ -0,0 +1,93 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let currentPage: number = 1;
+    const pages: NodeListOf<Element> = document.querySelectorAll('.page');
+    const links: NodeListOf<Element> = document.querySelectorAll('.page-link');
+    const prevPageLinks: NodeListOf<Element> = document.querySelectorAll('#prev-page-top, #prev-page-bottom');
+    const nextPageLinks: NodeListOf<Element> = document.querySelectorAll('#next-page-top, #next-page-bottom');
+    const total_pages: number = parseInt(document.querySelector('#pagination-top')!.getAttribute('data-total-pages')!);
+
+    const images: NodeListOf<HTMLImageElement> = document.querySelectorAll('img[data-src]');
+    const config: IntersectionObserverInit = {
+        rootMargin: '0px 0px 50px 0px',
+        threshold: 0
+    };
+
+    const imageObserver: IntersectionObserver = new IntersectionObserver(function(entries, self) {
+        entries.forEach(function(entry) {
+            if (entry.isIntersecting) {
+                preloadImage(entry.target as HTMLImageElement);
+                self.unobserve(entry.target);
+            }
+        });
+    }, config);
+
+    images.forEach(function(image) {
+        imageObserver.observe(image);
+    });
+
+    function preloadImage(img: HTMLImageElement) {
+        const src: string | null = img.getAttribute('data-src');
+        if (!src) {
+            return;
+        }
+        img.src = src;
+    }
+
+    function showPage(page: number) {
+        pages.forEach(function(pageDiv: Element) {
+            pageDiv.style.display = pageDiv.getAttribute('data-page') == page.toString() ? 'block' : 'none';
+        });
+        currentPage = page;
+        prevPageLinks.forEach(function(link: Element) {
+            link.style.visibility = currentPage == 1 ? 'hidden' : 'visible';
+        });
+        nextPageLinks.forEach(function(link: Element) {
+            link.style.visibility = currentPage == total_pages ? 'hidden' : 'visible';
+        });
+
+        // Remove the current-page class from all links
+        links.forEach(function(link: Element) {
+            link.classList.remove('current-page');
+        });
+
+        // Add the current-page class to the current page links
+        const currentPageLinks: NodeListOf<Element> = document.querySelectorAll('.page-link[data-page="' + currentPage + '"]');
+        if (currentPageLinks) {
+            currentPageLinks.forEach(function(link: Element) {
+                link.classList.add('current-page');
+            });
+        }
+    }
+
+    links.forEach(function(link: Element) {
+        link.addEventListener('click', function(e: Event) {
+            e.preventDefault();
+            const page: number = parseInt(link.getAttribute('data-page')!); // Convert the page attribute to a number
+            if (page) {
+                showPage(page);
+            }
+        });
+    });
+
+    prevPageLinks.forEach(function(link: Element) {
+        link.addEventListener('click', function(e: Event) {
+            e.preventDefault();
+            if (currentPage > 1) showPage(currentPage - 1);
+        });
+    });
+
+    nextPageLinks.forEach(function(link: Element) {
+        link.addEventListener('click', function(e: Event) {
+            e.preventDefault();
+            if (currentPage < total_pages) showPage(currentPage + 1);
+        });
+    });
+    showPage(currentPage);
+});
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
