79c53102e2e12d188081482581ad6ab52237f83a
Steven Goulet <steven.goulet@live.com>
2024-11-25 16:58:18 -0500

    made it a PWA

---
+++ weather/service-worker.js
@@ -0,0 +1,17 @@
+﻿self.addEventListener('install', event => {
+    console.log('Service worker installing...');
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+    console.log('Service worker activating...');
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
